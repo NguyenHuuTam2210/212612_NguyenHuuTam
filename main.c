4f777df395e3db2754b9e4292b68582e58a85b61
Tammmm <Nguyentamcd2210@gmail.com>
2024-10-24 10:13:34 +0700

    Them tat ca len sever

---
+++ main.c
@@ -0,0 +1,11 @@
+#include «stdio.h>
+
+//Khai bao dung ham tu cac file .c khac
+void print_shellc1();
+void print_shellc2();
+
+int main(){
+ print_shellc1(); // Goi ham tu shellc1.c
+ print_shellc2(); // Goi ham tu shellc2.c
+ return 0;
+}
